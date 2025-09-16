ce63f0104022d99f155f13e5d3b73ff486d4ada6
user <user@WIN-CP209TC39O5>
2025-09-17 02:58:43 +0300

    Operator+ CONST trouble

--- List/list.cpp
+++ List/list.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-using namespace std;
+using std::cin;
+using std::endl;
+using std::cout;
 
 #define tab "\t"
 #define delimeter "\n----------------------------------------\n"
@@ -26,17 +28,155 @@ class List
 
 	size_t size;   //Размер списка. size_t - это typedef на 'unsigned int.
 public:
+	class Iterator
+	{
+		Element* Temp;
+	public:
+		Iterator(Element* Temp = nullptr) :Temp(Temp) {}
+		~Iterator() {}
+
+		Iterator& operator++()
+		{
+			Temp = Temp->pNext;
+			return *this;
+		}
+		Iterator operator++(int)
+		{
+			Iterator old = *this;
+			Temp = Temp->pNext;
+			return old;
+		}
+		Iterator& operator--()
+		{
+			Temp = Temp->pPrev;
+			return *this;
+		}
+		Iterator operator--(int)
+		{
+			Iterator old = *this;
+			Temp = Temp->pNext;
+			return old;
+		}
+
+		//Comparison operators:
+		bool operator == (const Iterator& other)const
+		{
+			return this->Temp == other.Temp;
+		}
+		bool operator != (const Iterator& other)const
+		{
+			return this->Temp != other.Temp;
+		}
+		int operator*()const
+		{
+			return Temp->Data;
+		}
+		int& operator*()
+		{ 
+			return Temp->Data;
+		}
+
+	};
+	class ReverseIterator
+	{
+		Element* Temp;
+	public:
+		ReverseIterator(Element* Temp = nullptr) :Temp(Temp) {}
+		~ReverseIterator() {}
+
+		//Increment / Decrement:
+		ReverseIterator& operator++()
+		{
+			Temp = Temp->pPrev;
+			return *this;
+		}
+		ReverseIterator& operator++(int)
+		{
+			ReverseIterator old = *this;
+			Temp = Temp->pPrev;
+			return old;
+		}
+		ReverseIterator& operator--()
+		{
+			Temp = Temp->pNext;
+			return *this;
+		}
+		ReverseIterator operator--(int)
+		{
+			ReverseIterator old = *this;
+			Temp = Temp->pNext;
+			return old;
+		}
+
+		//Comparison operators:
+		bool operator == (const ReverseIterator& other)const
+		{
+			return this->Temp == other.Temp;
+		}
+		bool operator !=(const ReverseIterator& other)const
+		{
+			return this->Temp != other.Temp;
+		}
+		int operator*()const
+		{
+			return Temp->Data;
+		}
+		int& operator*()
+		{
+			return Temp->Data;
+		}
+	};
+
+	const Iterator begin() const
+	{
+		return Head;
+	}
+	const Iterator end() const
+	{
+		return nullptr;
+	}
+	ReverseIterator rbegin()
+	{
+		return Tail;
+	}
+	ReverseIterator rend()
+	{
+		return nullptr;
+	}
 	List()
 	{
 		Head = Tail = nullptr;
 		size = 0;
 		cout << "LConstructor\t" << this << endl;
 	}
+	List(const std::initializer_list<int>& il) :List()
+	{
+		for (int const* it = il.begin(); it != il.end(); ++it)
+		{
+			push_back(*it);
+		}
+		cout << "ILConstructor:\t" << this << endl;
+	}
+	List(const List& other) :List()
+	{
+		*this = other;
+		cout << "LCopyConstructor:\t" << this << endl;
+	}
 	~List()
 	{
 		//while (Head)pop_front();
 		while (Tail)pop_back();
 		cout << "LDestructor\t" << this << endl;
+	}
+	//Operators:
+	List& operator = (const List& other)
+	{
+		if (this == &other) return *this;
+		while (Head)pop_front();
+		for (Element* Temp = other.Head; Temp; Temp = Temp->pNext)
+			push_back(Temp->Data);
+		cout << "LCopyAssignment:\t" << this << endl;
+		return *this;
 	}
 
 	// Adding elements:
@@ -188,6 +328,14 @@ public:
 	}
 };
 
+List operator+ (const List& left, const List& right)
+{
+	List fusion = left;
+	for (List::Iterator it = right.begin(); it != right.end(); ++it)
+		fusion.push_back(*it *=100);
+	return fusion;
+}
+
 //#define BASE_CHECK
 #define HOME_WORK
 
@@ -228,8 +376,24 @@ void main()
 #ifdef HOME_WORK
 	List list1 = { 3,5,8,13,21 };
 	List list2 = { 34,55,89 };
+
+	//list1.print();
+	//list2.print();
+
 	List list3 = list1 + list2;
 	for (int i : list1)cout << i << tab; cout << endl;
 	for (int i : list2)cout << i << tab; cout << endl;
 	for (int i : list3)cout << i << tab; cout << endl;
+	for (List::Iterator it = list1.begin(); it != list1.end(); ++it)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
+	for (List::ReverseIterator it = list1.rbegin(); it != list1.rend(); ++it)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
+
 #endif // HOME_WORK
+}
